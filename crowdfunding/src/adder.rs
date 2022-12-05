f03186dd50ae52b2d5ccfac2b7414db58afef8e2
Romain MARTINEAU <romain@nexos.bzh>
2022-12-05 18:34:08 +0100

    wip

--- crowdfunding/src/adder.rs
+++ crowdfunding/src/adder.rs
@@ -22,6 +22,7 @@ pub trait Adder { // TODO @RMA rename it
     #[storage_mapper("enigma")]
     fn enigma(&self) -> SingleValueMapper<BigUint>;
 
+    // TODO
     // #[storage_mapper("progress")]
     // fn progress(&self) -> LinkedListMapper<IPlayer>;
 
@@ -29,18 +30,24 @@ pub trait Adder { // TODO @RMA rename it
     fn init(&self, phase: BigUint, enigma: BigUint) {
         self.phase().set(phase);
         self.enigma().set(enigma);
-
-        // get_sc_balance
     }
 
+    #[payable("*")]
     #[endpoint]
     fn update_progress(&self) {
-        // TODO @RMA check if player has NFT
+        require!(self.is_ticket_valid(), "Unable to update progress: Invalid ticket state");
+        // let _caller = self.blockchain().get_caller();
+        // let token_id = TokenIdentifier::from(""); // TODO get NFT id
+        // let _nft = self.blockchain().get_esdt_balance(&_caller, &token_id, 1);
+
+        // TODO fetch NFT from wallet get_sc_balance
         // TODO @RMA update NFT
         // TODO @RMA update progress
     }
 
     fn is_ticket_valid(&self) -> bool {
-        return true;
+        // TODO @RMA check if player has NFT
+        // TODO @RMA check if player is is at step
+        return false;
     }
 }
